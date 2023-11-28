6efab04c15d71703fb349ad382f35cced5d63dd6
Eunice <jes.e.0927@gmail.com>
2023-11-28 10:27:48 +0900

    Feat: SettigView 구성
    접근권한, 음소거, 다크모드 Toggle로 구성
    소리 볼륨 Slider로 구성

--- Lab3WalkingTamagotchi/SettingView.swift
+++ Lab3WalkingTamagotchi/SettingView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var healthAccess: Bool = false
+    @State var soundOff: Bool = false
+    @State var darkmode: Bool = false
+    @State var sound: Double = 2
+    
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: HealthDataAccessView()) { Text("접근 권한 설정") }
+            VStack(alignment: .leading) {
+                Text("설정")
+                    .font(.system(size: 45))
+                    .fontWeight(.heavy)
+                
+                HStack {
+                    Toggle(isOn: $healthAccess) {
+                        Text("건강 앱 접근 권한")
+                            .fontWeight(.bold)
+                    }
+                }
+                .padding(.vertical, 5)
+                Divider()
+                
+                HStack {
+                    Toggle(isOn: $soundOff) {
+                        Text("음소거")
+                            .fontWeight(.bold)
+                    }
+                }
+                .padding(.vertical, 5)
+                Divider()
+                
+                HStack {
+                    Toggle(isOn: $darkmode) {
+                        Text("다크모드")
+                            .fontWeight(.bold)
+                    }
+                }
+                .padding(.vertical, 5)
+                Divider()
+
+                Text("사운드")
+                    .font(.system(size: 30))
+                    .fontWeight(.heavy)
+                    .padding(.top, 40)
+                
+                Slider(value: $sound, in: 0...10, label: { Text("") }, minimumValueLabel: { Text("0") }, maximumValueLabel: { Text("10") })
+                
+                Spacer()
             }
-            .navigationTitle("설정")
+            .padding(30)
         }
     }
 }
